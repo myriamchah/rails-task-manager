6f46d0f871d755e02eaa9622daace2e6501d12ae
Myriam Chahlafi <myriam.chah@gmail.com>
2019-05-17 16:03:34 +0200

    all actions ok refacto missing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+  resources:tasks
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#index'
+#  get 'tasks/new', to: 'tasks#new'
+ # post 'tasks', to: 'tasks#create'
+
+ # get 'tasks', to: 'tasks#index'
+#  get 'tasks/:id', to: 'tasks#show', as: 'task'
+#
+ # get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+ # patch 'tasks/:id', to: 'tasks#update'
+
+ # delete 'tasks/:id', to: 'tasks#destroy'
 end
